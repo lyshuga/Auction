166c0d8f7b72cdcd67477b971dbffe96265711ac
lyshuga <kolia.liashuga@gmail.com>
2018-05-22 08:50:32 +0300

    Added image field to lotdto

--- Web/Auction.BLL/DTO/LotDTO.cs
+++ Web/Auction.BLL/DTO/LotDTO.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Auction.BLL.DTO
 {
@@ -13,6 +14,7 @@ namespace Auction.BLL.DTO
         public string Name { get; set; }
         public string Description { get; set; }
         public GoodType GoodType { get; set; }
+        public Image Photo { get; set; }
         public decimal Price { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime ExpireDate { get; set; }
