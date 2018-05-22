ae6f0d211d937ec495f29917eaa74afbffe03b6e
lyshuga <kolia.liashuga@gmail.com>
2018-05-22 08:46:30 +0300

    Init DTOs

---
+++ Web/Auction.BLL/DTO/LotDTO.cs
@@ -0,0 +1,21 @@
+﻿using Auction.BLL.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction.BLL.DTO
+{
+    public class LotDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public GoodType GoodType { get; set; }
+        public decimal Price { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public DateTime LastBid { get; set; }
+    }
+}
---
+++ Web/Auction.BLL/DTO/UserDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Auction.BLL.DTO
+{
+    public class UserDTO
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        public decimal Balance { get; set; }
+        public string CreditCard { get; set; }
+    }
+}
